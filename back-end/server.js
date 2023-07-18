02e3a56132a8b93c2f278b9f4ceac5f7697e05cb
Meir Chechik <meirchechik2015@gmail.com>
2023-07-18 10:23:58 +0300

    product route

--- back-end/server.js
+++ back-end/server.js
@@ -14,6 +14,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use("/api/users", userRoutes);
 app.use("/api/seed", seedRouter);
 
