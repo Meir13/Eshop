8cd0ae7e582a666e9dac82c00bb89247318ab4da
Meir Chechik <meirchechik2015@gmail.com>
2023-07-19 08:57:27 +0300

    switching to real data from mongo

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,7 @@
 import NavBar from "./Components/NavBar";
 import HomePage from "./Pages/HomePage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
 function App() {
   return (
     <>
--- front-end/src/Pages/HomePage.js
+++ front-end/src/Pages/HomePage.js
@@ -1,17 +1,24 @@
-import data from "../data";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Products from "../Components/Products";
+import "./HomePage.css";
 
 function HomePage() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const res = (await axios.get("/products")).data;
+      setProducts(res);
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <>
       <h1>Products</h1>
       <div className="products">
-        {data.products.map((product) => (
-          <div key={product.token} className="product">
-            <img alt={product.name} src={product.image} width={200}></img>
-            <p>{product.name}</p>
-            <p>{product.price}$</p>
-          </div>
-        ))}
+        <Products products={products}></Products>
       </div>
     </>
   );
